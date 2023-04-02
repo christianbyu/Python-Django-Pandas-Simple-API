8b47f844e99e60938279f87cefc9ae145734afda
pc aing <christianbayu@google.com>
2023-04-02 21:28:19 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Python-Django-Pandas-Simple-API
+
+## Installation
+
+1. Install Flask Library
+```
+pip install flask
+```
+
+2. Install Pandas Library
+```
+pip install pandas
+```
+
+## Run Aplication
+
+```
+python app.py
+```
--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/api/csv')
 def get_csv_data():
-    df = pd.read_csv('sample_data.csv')
+    df = pd.read_csv('./data/sample_data.csv')
     return jsonify(df.to_dict(orient='records'))
 
 @app.route('/api/newsapi')
 def get_news_data():
-    df = pd.read_csv('company_sales_data.csv')
+    df = pd.read_csv('./data/company_sales_data.csv')
     return jsonify(df.to_dict(orient='records'))
 
 if __name__ == '__main__':
